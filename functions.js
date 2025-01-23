b4995ed9042542f8412a45dc72dc1901016cfc70
Kelvin Cheng <z5487396@ad.unsw.edu.au>
2025-01-23 21:46:08 +1100

    separated header into js file for reusability

--- functions.js
+++ functions.js
@@ -20,5 +20,24 @@ function getAnswer() {
 }
 
 function redirectToCountdown() {
-    window.location.href="/countdown/index.html";
+    window.location.href="/countdown";
 }
+
+class Header extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <header class="blog-header py-2">
+                <div class="nav-scroller py-1 mb-1">
+                    <div class="nav d-flex" id="headerNav">
+                        <a class="p-1 text-white" href="/">Home</a>
+                        <a class="p-1 text-white" href="/projects">Projects</a>
+                        <a class="p-1 text-white" href="/unsw">UNSW</a>
+                    </div>
+                </div>
+            </header>
+        `;
+    }
+}
+
+customElements.define('main-header', Header);
+
